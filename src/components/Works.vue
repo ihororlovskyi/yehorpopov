933cfabbbe341aad17feb65b1a68d13ef32ee603
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2019-01-15 23:26:49 +0200

    upd

--- src/components/Works.vue
+++ src/components/Works.vue
@@ -1,12 +1,22 @@
 <template>
-  <v-layout row wrap class="Works mb-5" align-center>
+  <section>
+
+  <div v-if="loading" class="text-xs-center">
+    <v-progress-circular
+      indeterminate
+      :size="60"
+      color="black"
+    />
+  </div>
+
+  <v-layout v-else row wrap class="Works mb-5" align-center v-if="username">
     <v-flex xs6>
       <div class="fs24 fw800 mb-4">{{ title }}</div>
-      <div><a :href="instapage" target="_blank">@{{ username }}</a></div>
+      <div v-if="username"><a :href="instapage" target="_blank">@{{ username }}</a></div>
       <div>{{ position }}</div>
     </v-flex>
     <v-flex xs6 class="text-xs-right" >
-      <v-btn outline class="mx-0">
+      <v-btn outline class="mx-0" :href="instapage" target="_blank">
         <v-icon left>{{ btnFollowIcon }}</v-icon>
         {{ btnFollowText }}
       </v-btn>
@@ -31,12 +41,18 @@
     </v-flex>
 
   </v-layout>
+  </section>
+
 </template>
 
 <script>
   import axios from 'axios'
+  // import fetch from 'isomorphic-fetch'
 
   export default {
+    // props: [
+    //   'data'
+    // ],
     data () {
       return {
         btnFollowText: 'Follow',
@@ -46,13 +62,20 @@
         btnLoadText: 'Подзагрузить',
         btnLoadIcon: 'mdi-chevron-down',
 
+        // access_token: '176908350.1677ed0.3c30032e917a430e8d1f65eae2223b1e',
+        // url: 'https://api.instagram.com/v1/users/self/media/recent/',
+        // username: this.data.data[0].user.username,
+        // grams: this.data.data,
+        // next_url: this.data.pagination.next_url,
+        // error: false
         access_token: '176908350.1677ed0.3c30032e917a430e8d1f65eae2223b1e',
         url: 'https://api.instagram.com/v1/users/self/media/recent/',
         username: '',
         grams: [],
         next_url: '',
+        loading: true,
         error: false
-     }
+      }
     },
     computed: {
       instapage() {
@@ -66,6 +89,7 @@
             this.grams = data.data
             this.username = data.data[0].user.username
             this.next_url = data.pagination.next_url
+            this.loading = false
           })
           .catch(function (error) {
             console.log(error)
@@ -85,7 +109,7 @@
       }
     },
     created() {
-      this.getGrams();
+      this.getGrams()
     }
   }
 </script>
--- src/pages/works.vue
+++ src/pages/works.vue
@@ -6,11 +6,19 @@
 
 <script>
   import Works from '@/components/Works'
+  // import fetch from 'isomorphic-fetch'
 
   export default {
     components: {
       Works
     },
+    // async asyncData() {
+    //   const responseWorks = await fetch('https://api.instagram.com/v1/users/self/media/recent/?access_token=176908350.1677ed0.3c30032e917a430e8d1f65eae2223b1e')
+    //   const works = await responseWorks.json()
+    //   return {
+    //     works
+    //   }
+    // },
     head: {
       title: 'Works',
       meta: [
